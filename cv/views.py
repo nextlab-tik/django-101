96bc2c2113db3f04fe08d3e668f1b84a42d9db0e
Moez Bouhlel <bmoez.j@gmail.com>
2018-03-23 21:16:42 +0100

    update cv

--- cv/views.py
+++ cv/views.py
@@ -9,8 +9,13 @@ def cv(request, username='lejenome'):
     print(__file__)
     basedir = os.path.dirname(__file__)
     cv_path = os.path.join(basedir, "data", username + ".json")
-    if not os.path.isfile(cv_path):
-        raise Http404("cv not found")
-    with open(cv_path) as f:
-        data = json.load(f)
+    # if not os.path.isfile(cv_path):
+    #     raise Http404("cv not found")
+    # with open(cv_path) as f:
+    #     data = json.load(f)
+    try:
+        with open(cv_path) as f:
+            data = json.load(f)
+    except:
+         raise Http404("cv not found")
     return render(request, "cv.html", data)
